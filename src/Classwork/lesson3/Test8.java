455bada57c8b724fb91f8b2d026063fa47987bfe
114-15\user <lexabobko@gmail.com>
2019-10-08 21:21:30 +0300

    sssss

---
+++ src/Classwork/lesson3/Test8.java
@@ -0,0 +1,19 @@
+package Classwork.lesson3;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		int x = 0;
+		int a = 2;
+		int b = 5;
+		if (a < b) {
+			x = b - a;
+
+		} else {
+			x = a - b;
+		}
+		System.out.println(x);
+
+	}
+
+}
