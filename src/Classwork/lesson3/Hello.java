dfb40c0cbf7f07be4e232dad4169e493ca42c3de
114-15\user <lexabobko@gmail.com>
2019-10-08 19:57:24 +0300

    no message

---
+++ src/Classwork/lesson3/Hello.java
@@ -0,0 +1,20 @@
+package Classwork.lesson3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Hello {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("input username: ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String userName = reader.readLine();
+
+		System.out.println("Hello, " + userName);
+
+		}
+	}
+
+
