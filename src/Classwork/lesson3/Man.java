1664989f0478e4a50b36b480b2d093de760b89cb
114-15\user <lexabobko@gmail.com>
2019-10-08 19:25:20 +0300

    no message

---
+++ src/Classwork/lesson3/Man.java
@@ -0,0 +1,24 @@
+package Classwork.lesson3;
+
+public class Man {
+
+	public static void main(String[] args) {
+
+		class Men {
+			int hairLength;
+
+			public Men(int hairLength) {
+				super();
+				this.hairLength = hairLength;
+			}
+
+			Men men = new Men(20);
+
+			public Men() {
+				hairLength = 10;
+			}
+
+			Men men1 = new Men();
+		}
+	}
+}
