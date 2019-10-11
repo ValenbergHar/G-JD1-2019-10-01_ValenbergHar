39daef6becc962fa9077f6732f78bde0426943b4
114-15\user <lexabobko@gmail.com>
2019-10-11 21:41:35 +0300

    вав

--- src/Classwork/lesson4/Test13.java
+++ src/Classwork/lesson4/Test13.java
@@ -3,24 +3,29 @@ package Classwork.lesson4;
 import java.util.Random;
 
 public class Test13 {
-	// генерация чисел
+	// генерация чисел рефакторнинг - изменеине кода
 
 	public static void main(String[] args) {
 		int[] array = new int[10];
 		Random rand = new Random();
-		for (int i = 0; i < array.length; i++) {
-		array[i] = rand.nextInt(10);
-		}
+		extracted(array, rand);
 		System.out.println("initial array:");
 		for (int i = 0; i < array.length; i++) {
-		System.out.print(array[i] + " ");
+			System.out.print(array[i] + " ");
 		}
 		System.out.println();
 		for (int i = 0; i < array.length; i++) {
-		array[i] += 10;
+			array[i] += 10;
 		}
 		System.out.println("new array:");
 		for (int i = 0; i < array.length; i++) {
-		System.out.println(array[i] + " ");
-		}}}
-		
+			System.out.print(array[i] + " ");
+		}
+	}
+
+	private static void extracted(int[] array, Random rand) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(10);
+		}
+	}
+}
