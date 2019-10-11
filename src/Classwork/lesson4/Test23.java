81db3475e881561f54528093b704eb6fea137b61
114-15\user <lexabobko@gmail.com>
2019-10-11 20:47:21 +0300

    wewe

---
+++ src/Classwork/lesson4/Test23.java
@@ -0,0 +1,13 @@
+package Classwork.lesson4;
+
+public class Test23 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("count is: " + i);
+
+		}
+	}
+
+}
+
