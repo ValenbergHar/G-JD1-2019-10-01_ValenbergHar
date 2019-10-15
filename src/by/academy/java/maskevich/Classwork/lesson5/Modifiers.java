d7931ec7dc1faeda9a9a560317796daa5499936c
114-15\user <lexabobko@gmail.com>
2019-10-15 21:16:59 +0300

    no message

---
+++ src/by/academy/java/maskevich/Classwork/lesson5/Modifiers.java
@@ -0,0 +1,10 @@
+package by.academy.java.maskevich.Classwork.lesson5;
+
+public class Modifiers {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/by/academy/java/maskevich/Homework/Strr.java
+++ src/by/academy/java/maskevich/Homework/Strr.java
@@ -10,35 +10,43 @@ public class Strr {
 		int kolich = in.nextInt();
 
 		int mas[] = new int[kolich];
+		int numbers[] = new int[kolich];
+		
 		System.out.print("Введите числа: ");
 		for (int i = 0; i < kolich; i++) {
 			mas[i] = in.nextInt();
-			int numbers[] = new int[kolich];
-		}
-		System.out.print("Полученный массив: ");
-		for (int i = 0; i < mas.length; i++) {
-			System.out.print(mas[i] + " ");
-		}
-		System.out.print("\\n_____________");
-
-		// вывести на консоль самое короткое и самое длинное число,
-		// а также их длину. если чисел с одинаковой длиной
-		// несколько - вывести первое по очереди
-
-		int min = mas[0];
-		int r1 = 0;
-
-		
-		int maxlength = 0;
-		for (int i = 1; i < mas.length; i++) {
-			min = mas[i];
-			String str = Integer.toString(min);
-//			int maxlength = str.length();
-		//	System.out.println(min + " " + r11);
+						
 			
+			numbers[i] = (mas[i] + "").length();
+			String str = Integer.toString(mas[i]);
+			int maxlength = str.length();
 			
+			System.out.println(mas[i] + " " + numbers[i]+ " " +  maxlength);
+//		}
+//		System.out.print("Полученный массив: ");
+//		for (int i = 0; i < mas.length; i++) {
+//			System.out.print(mas[i] + " ");
+//		}
+//		System.out.print("\\n_____________");
+//
+//		// вывести на консоль самое короткое и самое длинное число,
+//		// а также их длину. если чисел с одинаковой длиной
+//		// несколько - вывести первое по очереди
+//
+//		int min = mas[0];
+//		int r1 = 0;
+//
+//		
+//		int maxlength = 0;
+//		for (int i = 1; i < mas.length; i++) {
+//			min = mas[i];
+//			String str = Integer.toString(min);
+////			int maxlength = str.length();
+//		//	System.out.println(min + " " + r11);
+//			
+//			
+//		}
+//		System.out.println(min + " " + maxlength);
 		}
-		System.out.println(min + " " + maxlength);
 	}
-
 }
