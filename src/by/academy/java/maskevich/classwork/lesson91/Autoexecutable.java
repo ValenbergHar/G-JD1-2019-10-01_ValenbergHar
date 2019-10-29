d42997f0a010fef7ad190379a869155edb296b53
AlexeyBobko <lexabobko@gmail.com>
2019-10-29 20:27:52 +0300

    rtrtr

---
+++ src/by/academy/java/maskevich/classwork/lesson91/Autoexecutable.java
@@ -0,0 +1,13 @@
+package by.academy.java.maskevich.classwork.lesson91;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Autoexecutable {
+
+}
+
