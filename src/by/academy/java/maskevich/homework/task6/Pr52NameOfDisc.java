9448b69c13ee21f5a2ba9705aca2eeaf73cce550
ValenbergHar <maskevich.valeri@gmail.com>
2019-11-29 16:32:07 +0300

    нгнг

---
+++ src/by/academy/java/maskevich/homework/task6/Pr52NameOfDisc.java
@@ -0,0 +1,36 @@
+package by.academy.java.maskevich.homework.task6;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Pr52NameOfDisc {
+
+	public static void main(String[] args) {
+		System.out.printf("\nEnter disc name" + ": ");
+		Scanner in = new Scanner(System.in);
+		String n = in.next();
+		for (int i = 0; i < 5; i++) {
+			showFilesAndDirectoryes(new File(n + ":\\"));
+		}
+	}
+
+	private static void showFilesAndDirectoryes(File file) {
+
+		File[] files = file.listFiles();
+		for (File p : files) {
+			if (!p.isDirectory()) {
+				System.out.printf("\n--Name of file: %s, length: %s bytes", p.getName(), p.length());
+			}
+			if (p.isDirectory()) {
+				try {
+					System.out.printf("\n-Name of directory: %s", p.getName());
+					showFilesAndDirectoryes(p);
+				} catch (Exception e) {
+
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+}
