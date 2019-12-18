8ed1138023db410dae7d9e8e5cd9da007c9b77ae
ValenbergHar <maskevich.valeri@gmail.com>
2019-12-19 00:13:46 +0300

    19.12.19

---
+++ src/myjava/snake/GameField.java
@@ -0,0 +1,20 @@
+package snake;
+
+import javax.swing.JPanel;
+
+import com.sun.prism.Image;
+
+public class GameField extends JPanel {
+	private final int SIZE = 320;
+	private final int DOT_SIZE = 16;
+	private final int ALL_DOTS = 400;
+	private Image dot;
+	private Image apple;
+	private int appleX;
+	private int appleY;
+	
+	public GameField() {
+		
+	}
+
+}
