60a48081b2c4128e45a0124e4b2fbf9f26b8629c
ValenbergHar <maskevich.valeri@gmail.com>
2019-12-19 16:51:57 +0300

    19.12.12/1

--- src/myjava/snake/GameField.java
+++ src/myjava/snake/GameField.java
@@ -1,7 +1,11 @@
 package snake;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
+import com.sun.glass.ui.Timer;
 import com.sun.prism.Image;
 
 public class GameField extends JPanel {
@@ -9,12 +13,30 @@ public class GameField extends JPanel {
 	private final int DOT_SIZE = 16;
 	private final int ALL_DOTS = 400;
 	private Image dot;
-	private Image apple;
+	private java.awt.Image apple;
 	private int appleX;
 	private int appleY;
-	
+	private int[] x = new int[ALL_DOTS];
+	private int[] y = new int[ALL_DOTS];
+	private int dots;
+	private Timer timer;
+	private boolean left = false;
+	private boolean right = true;
+	private boolean up = false;
+	private boolean down = false;
+	private boolean inGame = true;
+
 	public GameField() {
-		
+		setBackground(Color.BLACK);
+		loadImages();
+
+	}
+
+	public void loadImages() {
+		ImageIcon iia = new ImageIcon("apple.png");
+		apple = iia.getImage();
+		ImageIcon iid = new ImageIcon("dot.png");
+		apple = iid.getImage();
 	}
 
 }
