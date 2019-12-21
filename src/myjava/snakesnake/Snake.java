bb3af97f9569956aab8c1f296779a16a7043f5b3
ValenbergHar <maskevich.valeri@gmail.com>
2019-12-21 18:38:05 +0300

    19/12/19

--- src/myjava/snakesnake/Snake.java
+++ src/myjava/snakesnake/Snake.java
@@ -12,8 +12,8 @@ public class Snake {
 	final int POINT_RADIUS = 20;
 	final int FILED_WIDTH = 30;
 	final int FILED_HEIGHT = 20;
-	final int START_SNAKE_DX = 6;
-	final int START_SNAKE_DY = 28;
+	final int FILED_DX = 6;
+	final int FILED_DY = 28;
 	final int START_LOCATION = 200;
 	final int START_SNAKE_SIZE = 6;
 	final int START_SNAKE_X = 10;
@@ -40,7 +40,20 @@ public class Snake {
 	}
 
 	private void go() {
+		frame = new JFrame(TITLE_OF_PROGRAM + " : " + START_SNAKE_SIZE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(FILED_WIDTH * POINT_RADIUS + FILED_DX, FILED_HEIGHT * POINT_RADIUS + FILED_DY);
+		frame.setLocation(START_LOCATION, START_LOCATION);
+		frame.setResizable(false);
+		
+		frame.set
+	}
 
+	public class Canvas extends JPanel {
+		@Override
+		public void paint(Graphics g) {
+			super.paint(g);
+		}
 	}
 
 }
