474d29063e5d16d0af7a651408794bdfbc883392
ValenbergHar <maskevich.valeri@gmail.com>
2019-12-25 00:06:55 +0300

    25.12.19

--- src/myjava/swing/Calculator.java
+++ src/myjava/swing/Calculator.java
@@ -5,75 +5,96 @@ import java.awt.GridLayout;
 import java.awt.BorderLayout;
 
 public class Calculator {
-//Announcement of all calculator components.
-	JPanel windowContent;
-	JTextField displayField;
-	JButton buttonPoint;
-	JButton buttonEqual;
-	JPanel p1;
-	JPanel p2;
+// Объявляем и инициализируем компоненты окна
+	JButton button0 = new JButton("0");
+	JButton button1 = new JButton("1");
+	JButton button2 = new JButton("2");
+	JButton button3 = new JButton("3");
+	JButton button4 = new JButton("4");
+	JButton button5 = new JButton("5");
+	JButton button6 = new JButton("6");
+	JButton button7 = new JButton("7");
+	JButton button8 = new JButton("8");
+	JButton button9 = new JButton("9");
+	JButton buttonPoint = new JButton(".");
+	JButton buttonEqual = new JButton("=");
+	JButton buttonPlus = new JButton("+");
+	JButton buttonMinus = new JButton("-");
+	JButton buttonDivide = new JButton("/");
+	JButton buttonMultiply = new JButton("*");
+	JPanel windowContent = new JPanel();
+	JTextField displayField = new JTextField(30);
 
-// All components are created in the constructor
-	// and are added to the frame using a combination
-	// Borderlayout and Gridlayout
+// Конструктор
 	Calculator() {
-		windowContent = new JPanel();
-// We set the scheme for this panel
+// Установить менеджер расположения для панели
 		BorderLayout bl = new BorderLayout();
 		windowContent.setLayout(bl);
-// Create and display a field
-		// Add It To The North Area Of The Window
-		displayField = new JTextField(30);
+// Добавляем дисплей в верхней части окна
 		windowContent.add("North", displayField);
-// Create buttons using the constructor
-		// JButton class that accepts text
-		// buttons as a parameter
-
-		buttonPoint = new JButton(".");
-		buttonEqual = new JButton("=");
-// Create a panel with GridLayout
-		// which contains 12 buttons - 10 buttons with numbers
-		// and buttons with dot and equal sign
-		p1 = new JPanel();
+//Создаем панель с менеджером расположения GridLayout
+//в которой будет 12 кнопок - 10 цифр, и
+//кнопки “точка” и “равно”
+		JPanel p1 = new JPanel();
 		GridLayout gl = new GridLayout(4, 3);
 		p1.setLayout(gl);
-//Add buttons to p1 panel
-		for (int i = 0; i < 9; i++) {
-			p1.add(new JButton(Integer.toString(i + 1)));
-		}
+		p1.add(button1);
+		p1.add(button2);
+		p1.add(button3);
+		p1.add(button4);
+		p1.add(button5);
+		p1.add(button6);
+		p1.add(button7);
+		p1.add(button8);
+		p1.add(button9);
+		p1.add(button0);
 		p1.add(buttonPoint);
-		p1.add(new JButton("0"));
 		p1.add(buttonEqual);
-// Place panel p1 in the center area of the window
+//Добавляем панель p1 в центр окна
 		windowContent.add("Center", p1);
-
+//Создаем панель с менеджером расположения GridLayout
+//на которой будет 4 кнопки -
+//Плюс, Минус, Разделить и Умножить
 		JPanel p2 = new JPanel();
-		GridLayout g2 = new GridLayout(4, 1);
-		p2.setLayout(g2);
-		JButton buttonPlus = new JButton("+");
-		JButton buttonMinus = new JButton("-");
-		JButton buttonDivide = new JButton("/");
-		JButton buttonMultiply = new JButton("*");
+		GridLayout gl2 = new GridLayout(4, 1);
+		p2.setLayout(gl2);
 		p2.add(buttonPlus);
 		p2.add(buttonMinus);
 		p2.add(buttonMultiply);
 		p2.add(buttonDivide);
+//Добавляем панель p2 в правую часть окна
 		windowContent.add("East", p2);
-
-//Create a frame and set its main panel
+//Создаем frame и добавляем в него содержимое JFrame
 		JFrame frame = new JFrame("Calculator");
 		frame.setContentPane(windowContent);
-// make the window size sufficient
-		// in order to accommodate all componentsты
+//Устанавливаем размер окна, так чтобы уместились
+//все компоненты
 		frame.pack();
-// Finally, display the window
+//Показываем окно
 		frame.setVisible(true);
-		CalculatorEngine calcEngine = new CalculatorEngine();
+//Создаем экземпляр слушателя событий и
+//регистрируем его в каждой кнопке
+		CalculatorEngine calcEngine = new CalculatorEngine(this);
+		button0.addActionListener(calcEngine);
+		button1.addActionListener(calcEngine);
+		button2.addActionListener(calcEngine);
+		button3.addActionListener(calcEngine);
+		button4.addActionListener(calcEngine);
+		button5.addActionListener(calcEngine);
+		button6.addActionListener(calcEngine);
+		button7.addActionListener(calcEngine);
+		button8.addActionListener(calcEngine);
+		button9.addActionListener(calcEngine);
+		buttonPoint.addActionListener(calcEngine);
 		buttonPlus.addActionListener(calcEngine);
-
+		buttonMinus.addActionListener(calcEngine);
+		buttonDivide.addActionListener(calcEngine);
+		buttonMultiply.addActionListener(calcEngine);
+		buttonEqual.addActionListener(calcEngine);
 	}
 
 	public static void main(String[] args) {
+//Создаем экземпляр класса “Калькулятор”
 		Calculator calc = new Calculator();
 	}
 }
--- src/myjava/swing/CalculatorEngine.java
+++ src/myjava/swing/CalculatorEngine.java
@@ -8,16 +8,78 @@ import javax.swing.JOptionPane;
 
 public class CalculatorEngine implements ActionListener {
 
+	Calculator parent;
+	char selectedAction = ' '; // +, -, /, or *
+	double currentResult = 0;
+
+	// constructor saves calc's instatnce in var class
+	public CalculatorEngine(Calculator parent) {
+		this.parent = parent;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
 		// Get the event source
 		JButton clickedButton = (JButton) e.getSource();
-		// We get the inscription on the button
-		String clickedButtonLabel = clickedButton.getText();
+
+		String dispFieldText = parent.displayField.getText();
+		double displayValue = 0;
+		// We get text from the display
+		if (!"".equals(dispFieldText)) {
+			displayValue = Double.parseDouble(dispFieldText);
+		}
+		Object src = e.getSource();
+		// get button's info
+
+		if (src == parent.buttonPlus) {
+			selectedAction = '+';
+			currentResult = displayValue;
+			parent.displayField.setText("");
+		} else if (src == parent.buttonMinus) {
+			selectedAction = '-';
+			currentResult = displayValue;
+			parent.displayField.setText("");
+		} else if (src == parent.buttonDivide) {
+			selectedAction = '/';
+			currentResult = displayValue;
+			parent.displayField.setText("");
+		} else if (src == parent.buttonMultiply) {
+			selectedAction = '*';
+			currentResult = displayValue;
+			parent.displayField.setText("");
+		} else if (src == parent.buttonEqual) {
+			// —овершить арифметическое действие, в зависимости
+			// от selectedAction, обновить переменную currentResult
+			// и показать результат на дисплее
+			if (selectedAction == '+') {
+				currentResult += displayValue;
+				// —конвертировать результат в строку, добавл€€ его
+				// к пустой строке и показать его
+				parent.displayField.setText(" " + currentResult);
+			} else if (selectedAction == '-') {
+				currentResult -= displayValue;
+				parent.displayField.setText("" + currentResult);
+			} else if (selectedAction == '/') {
+				if(displayValue==0) {
+					parent.displayField.setText("" + "хер табе, а не дз€ленне на 0");
+				}else {
+				currentResult /= displayValue;
+				parent.displayField.setText("" + currentResult);}
+			} else if (selectedAction == '*') {
+
+				currentResult *= displayValue;
+				parent.displayField.setText("" + currentResult);
+			}
+		} else {
+			// ƒл€ всех цифровых кнопок присоединить надпись на
+			// кнопке к надписи в дисплее
+			String clickedButtonLabel = clickedButton.getText();
+			parent.displayField.setText(dispFieldText + clickedButtonLabel);
+		}
+
 		// Add the text on the button to the text of the message box
-		JOptionPane.showConfirmDialog(null, "You pressed " + clickedButtonLabel, "Just a test",
-				JOptionPane.PLAIN_MESSAGE);
+//		JOptionPane.showConfirmDialog(null, "You pressed " + dispFieldText, "Just a test", JOptionPane.PLAIN_MESSAGE);
 
 //	toast	
 //		JOptionPane.showConfirmDialog(null, "Something happened...", "Just a test", 
@@ -25,6 +87,4 @@ public class CalculatorEngine implements ActionListener {
 	}
 }
 
-
-
-// page 100
+// page 116
