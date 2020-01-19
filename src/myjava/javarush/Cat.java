5e6e0ba4a631aad4127de13f3f66373c84405eee
ValenbergHar <maskevich.valeri@gmail.com>
2020-01-19 22:02:53 +0300

    thtth

---
+++ src/myjava/javarush/Cat.java
@@ -0,0 +1,19 @@
+package javarush;
+
+public class Cat {
+	int i;
+	public Cat(int i) {
+		this.i = i;
+	}
+
+	public int getAge() {
+		return i;
+		
+	}
+
+	public void setAge(int j) {
+		this.i = i;
+		
+	}
+
+}
