51ea3f8b5a6f452451907c6cc2f69f9895fff3f9
ValenbergHar <maskevich.valeri@gmail.com>
2020-01-30 00:25:39 +0300

    fbgbgb

--- src/myjava/javarush/Cat.java
+++ src/myjava/javarush/Cat.java
@@ -1,19 +1,21 @@
 package javarush;
+public class Cat extends Animal {
 
-public class Cat {
-	int i;
-	public Cat(int i) {
-		this.i = i;
-	}
+   String tail = "Изначальное значение tail в классе Cat";
 
-	public int getAge() {
-		return i;
-		
-	}
+   static int catsCount = 37;
 
-	public void setAge(int j) {
-		this.i = i;
-		
-	}
+   public Cat(String brain, String heart, String tail) {
+       super(brain, heart);
+       System.out.println("Конструктор класса Cat начал работу (конструктор Animal уже был выполнен)");
+       System.out.println("Текущее значение статической переменной catsCount = " + catsCount);
+       System.out.println("Текущее значение статической переменной catsCount = " + animalCount);
+       System.out.println("Текущее значение tail = " + this.tail);
+       this.tail = tail;
+       System.out.println("Текущее значение tail = " + this.tail);
+   }
 
+   public static void main(String[] args) {
+       Cat cat = new Cat("Мозг", "Сердце", "Хвост");
+   }
 }
