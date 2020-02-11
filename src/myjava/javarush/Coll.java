eac7ba76beb6aa2c3b09f2a731f6ff7c3f7c6b00
ValenbergHar <maskevich.valeri@gmail.com>
2020-02-11 23:45:40 +0300

    fthfy

---
+++ src/myjava/javarush/Coll.java
@@ -0,0 +1,17 @@
+package javarush;
+
+public class Coll <T> {
+
+	private Integer integer = new Integer(5);
+	public static void main(String[] args) {
+		Coll<Integer> coll = new Coll<Integer>();
+		coll.integer;
+
+	}
+
+	private void setItem(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
