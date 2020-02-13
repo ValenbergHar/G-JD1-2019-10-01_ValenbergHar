7e5459510a050d353cd1d9fa87eb4fd1b59ccb87
ValenbergHar <maskevich.valeri@gmail.com>
2020-02-13 16:37:06 +0300

    верпрвпар

--- src/android/app/src/main/java/com/example/firstapp/MainActivity.java
+++ src/android/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,41 +1,53 @@
-package com.example.justjava;
+package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import java.text.NumberFormat;
-import java.util.Locale;
-
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int count;
+    private int quantity;
     private int price=10;
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void increment(View view) {
-        display(++count);
-        displayPrice(price*count);
+        display(++quantity);
 
     }
     public void decrement(View view) {
-        display(count<=0?count:--count);
-        displayPrice(price*count);
+        display(quantity<=0?quantity:--quantity);
+
     }
 
-        private void display ( int number){
+    private void display ( int number){
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-        }
+    }
 
 
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(""+number);
+        priceTextView.setText(createOrderSummary (name, quantity));
+        //priceTextView.setText(""+number);
         //(NumberFormat.getCurrencyInstance(new Locale("hi", "IN")).format(""+number));
     }
+
+    public void order(View view) {
+
+        displayPrice(price*quantity);
+        Toast.makeText(this, "Шчыры Дзяк!", Toast.LENGTH_SHORT).show();
     }
+
+    public String createOrderSummary (String name, int quantity){
+        return "Name: " + name + "\n" + "Quantity: " + quantity + "\n" + "Total: $" + price*quantity +"\n";
+    }
+
+
+
+}
