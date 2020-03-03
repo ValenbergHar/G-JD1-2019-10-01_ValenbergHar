12f713c6dfaeddeaf1c45c2697637b3f3c881207
ValenbergHar <maskevich.valeri@gmail.com>
2020-03-03 16:23:56 +0300

    митьитьимо

---
+++ src/myjava/headfirst1/chap02/GameLauncher.java
@@ -0,0 +1,8 @@
+package headfirst1.chap02;
+
+public class GameLauncher {
+    public static void main (String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
