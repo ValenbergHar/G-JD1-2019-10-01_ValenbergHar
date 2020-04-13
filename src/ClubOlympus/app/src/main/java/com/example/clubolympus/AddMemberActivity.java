8e99a36d5143aad01787bbfaff910b4c92e426d3
ValenbergHar <maskevich.valeri@gmail.com>
2020-04-13 16:52:35 +0300

    смисмтсмт

---
+++ src/ClubOlympus/app/src/main/java/com/example/clubolympus/AddMemberActivity.java
@@ -0,0 +1,39 @@
+package com.example.clubolympus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class AddMemberActivity extends AppCompatActivity {
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText groupNameEditText;
+    private Spinner genderSpinner;
+    private int gender = 0;
+    private ArrayAdapter spinnerAdapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_member);
+
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+        lastNameEditText = findViewById(R.id.lastNameEditText);
+        groupNameEditText = findViewById(R.id.groupEditText);
+        genderSpinner = findViewById(R.id.genderSpinner);
+
+
+
+        spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.array_gender, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(spinnerAdapter);
+
+    }
+}
