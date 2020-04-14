a98eb9fd9a4d11d735085946ebe2f102b7bbc85a
ValenbergHar <maskevich.valeri@gmail.com>
2020-04-14 16:49:05 +0300

    роролророл

--- src/ClubOlympus/app/src/main/java/com/example/clubolympus/AddMemberActivity.java
+++ src/ClubOlympus/app/src/main/java/com/example/clubolympus/AddMemberActivity.java
@@ -1,13 +1,20 @@
 package com.example.clubolympus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import java.util.ArrayList;
+import com.example.clubolympus.data.ClubOlympysContract;
+
 
 public class AddMemberActivity extends AppCompatActivity {
     private EditText firstNameEditText;
@@ -16,7 +23,6 @@ public class AddMemberActivity extends AppCompatActivity {
     private Spinner genderSpinner;
     private int gender = 0;
     private ArrayAdapter spinnerAdapter;
-
 
 
     @Override
@@ -30,10 +36,50 @@ public class AddMemberActivity extends AppCompatActivity {
         genderSpinner = findViewById(R.id.genderSpinner);
 
 
-
         spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.array_gender, android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(spinnerAdapter);
+        genderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedGender = (String) adapterView.getItemAtPosition(i);
+                if (!TextUtils.isEmpty(selectedGender)) {
+                    if (selectedGender.equals("Male")) {
+                        gender = ClubOlympysContract.MemberEntry.GENDER_MALE;
+                    } else if (selectedGender.equals("Female")) {
+                        gender = ClubOlympysContract.MemberEntry.GENDER_FEMALE;
+                    } else {
+                        gender = ClubOlympysContract.MemberEntry.GENDER_UNKNOWN;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                gender = 0;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_member_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_member:
+                return true;
+            case R.id.delete_member:
+                return true;
+            case android.R.id.home:
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
 
     }
 }
