181ac49946ccafa850b90e10f2141ed79ed92758
ValenbergHar <maskevich.valeri@gmail.com>
2020-04-23 23:54:37 +0300

    пиапипи

---
+++ src/myjava/web/example1/src/ServletGet.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletGet
+ */
+@WebServlet("/servletget")
+public class ServletGet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String oneString = request.getParameter("one");
+		String twoString = request.getParameter("two");
+		response.getWriter()
+				.write("<html>" + "<head></head>" + "<body>" + "one = " + oneString + " two = " + twoString
+						+ "<form action ='servletget' method = 'get'>" + "<input type = 'text' name = 'one'/>"
+						+ "<input type = 'text' name = 'two'/>" + "<input type = 'submit' name = 'submit'/>" + "</form>"
+						+ "</body>" + "</html>");
+	}
+
+}
