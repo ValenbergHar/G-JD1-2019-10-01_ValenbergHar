3e22a2344776cd608c295d3409fbff9f59d396f8
ValenbergHar <maskevich.valeri@gmail.com>
2020-05-08 17:29:00 +0300

    жлдоітол

--- src/myjava/senla/task6/Backpack.java
+++ src/myjava/senla/task6/Backpack.java
@@ -1,36 +1,27 @@
 package senla.task6;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.List;
 
-public class BackPack {
+public class Task6 {
 
 	public static void main(String[] args) {
-		final Item item1 = new Item(4, 40);
-		final Item item2 = new Item(5, 30);
-		final Item item3 = new Item(6, 8);
-		final Item item4 = new Item(10, 54);
 
-		final Item[] items = { item1, item2, item3,item4 };
+		List<Item> items = null;
 
-		Arrays.sort(items, Comparator.comparingDouble(Item::valueUnitOfWeight).reversed());
-		System.out.println(Arrays.toString(items));
-		final int W = 10;
-		int weightInBackpack = 0;
-		int valueInBackpack = 0;
-		int currentItem = 0;
+		items.add(new Item("Книга", 1, 600));
+		items.add(new Item("Бинокль", 2, 5000));
+		items.add(new Item("Аптечка", 4, 1500));
+		items.add(new Item("Ноутбук", 2, 40000));
+		items.add(new Item("Котелок", 1, 500));
 
-		while (currentItem < items.length && weightInBackpack != W) {
-			if (weightInBackpack + items[currentItem].getWeight() < W) {
-				valueInBackpack += items[currentItem].getValue();
-				weightInBackpack += items[currentItem].getWeight();
-			} else {
-				valueInBackpack += (W - weightInBackpack) / (double) items[currentItem].getWeight()*items[currentItem].getValue();
-				weightInBackpack=W;
-			}
-			currentItem++;
+		Backpack bp = new Backpack(10.1);
+		bp.makeAllSets(items);
+		List<Item> solve = bp.getBestSet();
+
+		if (solve == null) {
+			System.out.println("There is no result");
+		} else {
+			System.out.println(solve);
 		}
-		System.out.println( + valueInBackpack);
-
 	}
 }
