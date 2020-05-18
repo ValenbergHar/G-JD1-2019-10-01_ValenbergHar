a22cfbfbd4c18af679b10e6728212f3829a57f5c
ValenbergHar <maskevich.valeri@gmail.com>
2020-05-18 23:21:51 +0300

    cncgn

---
+++ src/spring/alishev/src/main/java/org/example/TestBean.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class TestBean {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+
+}
