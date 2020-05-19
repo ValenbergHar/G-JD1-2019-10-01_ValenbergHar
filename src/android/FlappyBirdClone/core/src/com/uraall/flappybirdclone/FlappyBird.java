8099e740c9ded85194374482cc22593cae56eb13
ValenbergHar <maskevich.valeri@gmail.com>
2020-05-19 20:48:35 +0300

    bvcnmnm

--- src/android/FlappyBirdClone/core/src/com/uraall/flappybirdclone/FlappyBird.java
+++ src/android/FlappyBirdClone/core/src/com/uraall/flappybirdclone/FlappyBird.java
@@ -7,27 +7,39 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture background;
+    Texture[] bird;
+    Texture bird_wings_up;
+    int birdStateFlag=0;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("background.png");
+        bird_wings_up = new Texture("bird_wings_up.png");
+        bird=new Texture[2];
+        bird[0]=new Texture("bird_wings_up.png");
+        bird[1]=new Texture("bird_wings_down.png");
+    }
+
+    @Override
+    public void render() {
+
+        if(birdStateFlag==0){
+            birdStateFlag=1;
+        }else{
+            birdStateFlag=0;
+        }
+
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(bird[birdStateFlag], Gdx.graphics.getWidth()/2 - bird[birdStateFlag].getWidth()/2, Gdx.graphics.getHeight()/2-bird[birdStateFlag].getHeight()/2);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
--- src/spring/alishev/src/main/java/org/example/TestSpring.java
+++ src/spring/alishev/src/main/java/org/example/TestSpring.java
@@ -5,16 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
-
-        for(int i=18; i<100; i++){
-
-            System.out.print(i+ ",");
-        }
-
-
-
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+        context.close();
 
     }
 }
