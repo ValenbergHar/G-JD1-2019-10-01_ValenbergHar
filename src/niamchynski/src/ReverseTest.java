0a768c6bbd764f625c4964212391c5639e30ce16
ValenbergHar <maskevich.valeri@gmail.com>
2020-05-31 23:40:19 +0300

    fghdg

---
+++ src/niamchynski/src/ReverseTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseTest {
+    @Test
+    public void test() {
+       assertEquals("Hello Word", Reverse.reverse("olleH droW"));
+    }
+
+
+}
