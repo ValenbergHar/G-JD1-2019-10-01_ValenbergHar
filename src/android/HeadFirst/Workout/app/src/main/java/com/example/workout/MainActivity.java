e0d4e97d36c11cd13a51714527fe2deb7d09d708
ValenbergHar <maskevich.valeri@gmail.com>
2020-06-12 23:48:57 +0300

    ывап

--- src/android/HeadFirst/Workout/app/src/main/java/com/example/workout/MainActivity.java
+++ src/android/HeadFirst/Workout/app/src/main/java/com/example/workout/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void onShowDetails(View view) {
         Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
         startActivity(intent);
     }
 }
--- src/android/HeadFirst/Workout/app/src/main/java/com/example/workout/WorkoutDetailFragment.java
+++ src/android/HeadFirst/Workout/app/src/main/java/com/example/workout/WorkoutDetailFragment.java
@@ -1,26 +1,19 @@
 package com.example.workout;
 
-
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class WorkoutDetailFragment extends Fragment {
     private long workoutId;
 
-
-    public WorkoutDetailFragment() {
-        // Required empty public constructor
-    }
-    public void setWorkout(long id) {
-        this.workoutId = id;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
     @Override
@@ -28,7 +21,6 @@ public class WorkoutDetailFragment extends Fragment {
         super.onStart();
         View view = getView();
         if (view != null) {
-
             TextView title = (TextView) view.findViewById(R.id.textTitle);
             Workout workout = Workout.workouts[(int) workoutId];
             title.setText(workout.getName());
@@ -37,12 +29,7 @@ public class WorkoutDetailFragment extends Fragment {
         }
     }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    public void setWorkout(long id) {
+        this.workoutId = id;
     }
-
 }
