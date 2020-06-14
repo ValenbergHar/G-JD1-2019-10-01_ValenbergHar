fdf62948f471b5ae57e649b989a0333bf8c1dc3d
ValenbergHar <maskevich.valeri@gmail.com>
2020-06-14 23:49:12 +0300

    asdfg

--- src/android/Geo/app/src/main/java/com/valenberg/MainActivity.java
+++ src/android/Geo/app/src/main/java/com/valenberg/MainActivity.java
@@ -1,14 +1,123 @@
- package com.valenberg;
+package com.valenberg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.valenberg.Models.User;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSignIn, btnRegister;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+    RelativeLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnRegister = findViewById(R.id.btnRegister);
+        root = findViewById(R.id.root_element);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterWindow();
+            }
+        });
+
     }
+
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Register");
+        dialog.setMessage("Put all information");
+//
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View registerWindow = inflater.inflate(R.layout.register_window, null, false);
+        dialog.setView(registerWindow);
+
+//        final MaterialEditText email = registerWindow.findViewById(R.id.emailField);
+//        final MaterialEditText password = registerWindow.findViewById(R.id.passField);
+//        final MaterialEditText name = registerWindow.findViewById(R.id.nameField);
+//        final MaterialEditText phone = registerWindow.findViewById(R.id.phoneField);
+
+//        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int which) {
+//                dialogInterface.dismiss();
+//            }
+//        });
+
+//        dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int which) {
+//                if (TextUtils.isEmpty(email.getText().toString())) {
+//                    Snackbar.make(root, "Put your post", Snackbar.LENGTH_SHORT).show();
+//                    return;
+//                }
+//
+//                if (TextUtils.isEmpty(name.getText().toString())) {
+//                    Snackbar.make(root, "Put your name", Snackbar.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                if (TextUtils.isEmpty(phone.getText().toString())) {
+//                    Snackbar.make(root, "Put your phone", Snackbar.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                if (password.getText().toString().length() < 5) {
+//                    Snackbar.make(root, "Put your password, more than 5 symbols", Snackbar.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+//                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                            @Override
+//                            public void onSuccess(AuthResult authResult) {
+//                                User user = new User();
+//                                user.setEmail(email.getText().toString());
+//                                user.setName(name.getText().toString());
+//                                user.setPassword(password.getText().toString());
+//                                user.setPhone(phone.getText().toString());
+//
+//                                users.child(user.getEmail())
+//                                        .setValue(user)
+//                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                                                                  @Override
+//                                                                  public void onSuccess(Void aVoid) {
+//                                                                      Snackbar.make(root, "User is added", Snackbar.LENGTH_SHORT).show();
+//                                                                  }
+//                                                              }
+//                                        );
+//                            }
+//                        });
+//
+//            }
+//        });
+
+    }
+
+
 }
