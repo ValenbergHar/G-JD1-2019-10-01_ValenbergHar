f0ba31adf95aa3fab535a84c22739d477045dca2
ValenbergHar <maskevich.valeri@gmail.com>
2020-06-18 20:11:48 +0300

    ывап

--- src/android/Geo/app/src/main/java/com/valenberg/MainActivity.java
+++ src/android/Geo/app/src/main/java/com/valenberg/MainActivity.java
@@ -3,6 +3,7 @@ package com.valenberg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -56,66 +57,67 @@ public class MainActivity extends AppCompatActivity {
         dialog.setTitle("Register");
         dialog.setMessage("Put all information");
 //
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View registerWindow = inflater.inflate(R.layout.register_window, null, false);
+      LayoutInflater inflater = LayoutInflater.from(this);
+       // LayoutInflater inflater=getLayoutInflater();
+        View registerWindow = inflater.inflate(R.layout.register_window, null);
         dialog.setView(registerWindow);
 
-//        final MaterialEditText email = registerWindow.findViewById(R.id.emailField);
-//        final MaterialEditText password = registerWindow.findViewById(R.id.passField);
-//        final MaterialEditText name = registerWindow.findViewById(R.id.nameField);
-//        final MaterialEditText phone = registerWindow.findViewById(R.id.phoneField);
+        final MaterialEditText email = registerWindow.findViewById(R.id.emailField);
+        final MaterialEditText password = registerWindow.findViewById(R.id.passField);
+        final MaterialEditText name = registerWindow.findViewById(R.id.nameField);
+        final MaterialEditText phone = registerWindow.findViewById(R.id.phoneField);
 
-//        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialogInterface, int which) {
-//                dialogInterface.dismiss();
-//            }
-//        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
 
-//        dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialogInterface, int which) {
-//                if (TextUtils.isEmpty(email.getText().toString())) {
-//                    Snackbar.make(root, "Put your post", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//
-//                if (TextUtils.isEmpty(name.getText().toString())) {
-//                    Snackbar.make(root, "Put your name", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                if (TextUtils.isEmpty(phone.getText().toString())) {
-//                    Snackbar.make(root, "Put your phone", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                if (password.getText().toString().length() < 5) {
-//                    Snackbar.make(root, "Put your password, more than 5 symbols", Snackbar.LENGTH_SHORT).show();
-//                    return;
-//                }
-//                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-//                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-//                            @Override
-//                            public void onSuccess(AuthResult authResult) {
-//                                User user = new User();
-//                                user.setEmail(email.getText().toString());
-//                                user.setName(name.getText().toString());
-//                                user.setPassword(password.getText().toString());
-//                                user.setPhone(phone.getText().toString());
-//
-//                                users.child(user.getEmail())
-//                                        .setValue(user)
-//                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-//                                                                  @Override
-//                                                                  public void onSuccess(Void aVoid) {
-//                                                                      Snackbar.make(root, "User is added", Snackbar.LENGTH_SHORT).show();
-//                                                                  }
-//                                                              }
-//                                        );
-//                            }
-//                        });
-//
-//            }
-//        });
+        dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if (TextUtils.isEmpty(email.getText().toString())) {
+                    Snackbar.make(root, "Put your post", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(name.getText().toString())) {
+                    Snackbar.make(root, "Put your name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(phone.getText().toString())) {
+                    Snackbar.make(root, "Put your phone", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.getText().toString().length() < 5) {
+                    Snackbar.make(root, "Put your password, more than 5 symbols", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user = new User();
+                                user.setEmail(email.getText().toString());
+                                user.setName(name.getText().toString());
+                                user.setPassword(password.getText().toString());
+                                user.setPhone(phone.getText().toString());
+
+                                users.child(user.getEmail())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                  @Override
+                                                                  public void onSuccess(Void aVoid) {
+                                                                      Snackbar.make(root, "User is added", Snackbar.LENGTH_SHORT).show();
+                                                                  }
+                                                              }
+                                        );
+                            }
+                        });
+
+            }
+        });
 
     }
 
