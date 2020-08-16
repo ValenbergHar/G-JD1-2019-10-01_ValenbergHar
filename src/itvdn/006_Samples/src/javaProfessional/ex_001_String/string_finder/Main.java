0b6616bd0a07bb953136ed6ee66b9aa9c18792f8
ValenbergHar <maskevich.valeri@gmail.com>
2020-08-16 14:26:57 +0300

    wertyui

--- src/itvdn/006_Samples/src/javaProfessional/ex_001_String/string_finder/Main.java
+++ src/itvdn/006_Samples/src/javaProfessional/ex_001_String/string_finder/Main.java
@@ -7,7 +7,7 @@ public class Main {
                 "Rahman said. An online petition has been posted by Java EE Guardians urging Oracle to move forward with EE or " +
                 "turn the work over to others. As of Tuesday, the petition had more than 3,200 signatures. " +
                 "Java EE Guardians has pondered developing enterprise-level Java improvements on its own.";
-        String[] array = str.split("[.,]");
+        String[] array = str.split("\\.");
         for (String temp : array){
             System.out.println(temp);
         }
